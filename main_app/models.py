3ebdf41457d431d621e2ecae5613eb63b0c0bdc6
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-03-23 18:34:43 -0700

    partial validations done, will continue tomorrow

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+import re
+
+class ShowsManager(models.Manager):
+    def basic_validator(self, post_data):
+        errors = {}
+
+        if 'title' not in post_data:
+            errors['title'] = "Show title should be at least 2 characters"
+        elif len(post_data['title']) < 2:
+            errors['title'] = "Show title should be at least 2 characters"
+
+        if 'network' not in post_data:
+            errors['network'] = "Show network should be at least 3 characters"
+        elif len(post_data['network']) < 3:
+            errors['network'] = "Show network should be at least 3 characters"
+
+        if 'desc' not in post_data:
+            errors['desc'] = "Show description should be at least 10 characters"
+        elif len(post_data['desc']) < 10:
+            errors['desc'] = "Show description should be at least 10 characters"
+
+        return errors
 
 # Create your models here.
 class Shows(models.Model):
@@ -8,6 +30,9 @@ class Shows(models.Model):
     desc = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    objects = ShowsManager()
 
     def __repr__(self):
         return f"Shows {self.title} {self.network} {self.release_date}"
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Shows
+from django.contrib import messages ##added for validations
+
+from .models import *
 
 
 def index(request):
@@ -12,38 +14,59 @@ def shows(request):
 def add_show(request):
     return render(request, 'add_new_show.html')
 
-
 def show_added(request):
-    added_show = Shows.objects.create(
-        title = request.POST["title"],
-        network = request.POST["network"],
-        release_date = request.POST["release_date"],
-        desc = request.POST["desc"]
-    )
+    print (request.POST)
+    errors = Shows.objects.basic_validator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        
+        return redirect("/shows/new")
+
+    else:
+        added_show = Shows.objects.create(
+            title = request.POST["title"],
+            network = request.POST["network"],
+            release_date = request.POST["release_date"],
+            desc = request.POST["desc"]
+        )
+        messages.success(request, "Show successfully added")
     print(request.POST)
     return redirect(f'/shows/{added_show.id}')
 
 def edit_show(request, show_id):
+
     context = {"shows": Shows.objects.get(id=show_id)}
     return render(request, 'edit_shows.html', context)
 
 def submit_edit(request, show_id):
-    edited_show = Shows.objects.get(id=show_id)
-    if request.POST['title']:
-        edited_show.title = request.POST['title']
+    print (request.POST)
+    errors = Shows.objects.basic_validator(request.POST)
+    print(errors)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
 
-    if request.POST['network']:
-        edited_show.network = request.POST['network']
+        return redirect(f'/shows/{edited_show.id}')
 
-    if request.POST['release_date']:
-        edited_show.release_date = request.POST['release_date']
+    else:
+        edited_show = Shows.objects.get(id=show_id)
+        if request.POST['title']:
+            edited_show.title = request.POST['title']
 
-    if request.POST['desc']:
-        edited_show.desc = request.POST['desc']
+        if request.POST['network']:
+            edited_show.network = request.POST['network']
+
+        if request.POST['release_date']:
+            edited_show.release_date = request.POST['release_date']
+
+        if request.POST['desc']:
+            edited_show.desc = request.POST['desc']
 
     edited_show.save()
     print(request.POST)
-    return redirect(f'/shows/{edited_show.id}')
+    return redirect(f'/shows/{edited_show.id}/edit')
 
 def show_listing(request, show_id):
     context = {"shows": Shows.objects.get(id=show_id)}
@@ -53,3 +76,11 @@ def delete_show(request, show_id):
     Shows.objects.get(id=show_id).delete()
     return redirect('/shows_list')
 
+def update(request, show_id):
+    errors = Shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{edited_show.id}') 
+##I need 2 of these for edited and also for adding a new one don't I..
+## if that's the case then the method would be the same just a different def name and diff redirect link i believve
